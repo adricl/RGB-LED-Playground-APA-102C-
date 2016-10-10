01d5c2c149d567d76539365c5e1bd5a01b2a897d
adric <adric.landman@gmail.com>
2016-10-10 22:20:13 +1100

    Fixed dumb bugs, works correctly now

--- ESPoi/ESPoi.ino
+++ ESPoi/ESPoi.ino
@@ -8,51 +8,37 @@
 
 #define PIN_DATA 4 //D2
 #define PIN_CLK 5 //D1
-#define BRIGHTNESS  40
+#define BRIGHTNESS  10
 #define LED_TYPE    APA102
 #define COLOR_ORDER BGR
-#define LED_STRIP_COUNT 30
-CRGB leds[LED_STRIP_COUNT];
 
-//SLICES FOR IMAGE
-#define NUM_SLICES  150
+//LED COUNT
+#define LED_STRIP_COUNT 30
+
+
+CRGB leds[LED_STRIP_COUNT];
 
 void setup() {
 
   delay( 300 ); // power-up safety delay
 
-
-  Serial.begin(57600);
-
-
   FastLED.addLeds<LED_TYPE, PIN_DATA, PIN_CLK, COLOR_ORDER>(leds, LED_STRIP_COUNT).setCorrection( TypicalLEDStrip );
   FastLED.setBrightness(  BRIGHTNESS );
-  Serial.print("\nINIT\n");
-  
-  //FastLED.showColor(CRGB::Black);
-  
-  
-  delay( 3000 ); 
+  delay( 300 ); 
 }
 
 void loop() {
   uint32_t currentTime = millis();
   static int slice = 0;
-
-
+  static int slice_count = (int) ( (sizeof(array1)/sizeof(int))/LED_STRIP_COUNT);
+  
   for(int led_num = 0; led_num < LED_STRIP_COUNT; led_num++){
-
-    uint8_t pos = slice * LED_STRIP_COUNT + led_num;
-    //Serial.printf("Slice: %d, LED: %d, pos: %d", slice, led_num, pos);   
-    leds[led_num] = array1[pos];
-    
-    //Serial.printf(" %x ", array1[pos]);
-    
+    int pos = led_num * slice_count + slice;
+    leds[led_num] = array1[pos];        
   }
-  //Serial.print("\n");
-  FastLED.show();
-  FastLED.delay(40);
-  if ( slice >= NUM_SLICES){
+  FastLED.delay(20);
+
+  if ( slice >= slice_count){
     slice = 0;
   }
   else {
